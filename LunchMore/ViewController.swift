cef3248a52f846c8ec96293ebde880243d9c4ece
Alonzo Thomas <lenell16@gmail.com>
2015-04-11 05:17:34 -0400

    Initial Commit

---
+++ LunchMore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LunchMore
+//
+//  Created by Alonzo Trove on 4/11/15.
+//  Copyright (c) 2015 Alonzo Trove. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
