48d57ad040dd4a2b55a09d8c65e505334b68e904
Alonzo Thomas <lenell16@gmail.com>
2015-04-15 17:32:54 -0400

    Initial commit

--- LunchMore/AppDelegate.swift
+++ LunchMore/AppDelegate.swift
@@ -7,17 +7,67 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("GyB3mdaqfGnlUhhkrEDl3WQKnOs878DLip55ez25",
+            clientKey: "cQ9a1BxTFOgom62GD3QrwvRQSJEKozomU1gJ9DjY")
+        
+        // Register for Push Notitications
+        if application.applicationState != UIApplicationState.Background {
+            // Track an app open here if we launch with a push, unless
+            // "content_available" was used to trigger a background push (introduced in iOS 7).
+            // In that case, we skip tracking here to avoid double counting the app-open.
+            
+            let preBackgroundPush = !application.respondsToSelector("backgroundRefreshStatus")
+            let oldPushHandlerOnly = !self.respondsToSelector("application:didReceiveRemoteNotification:fetchCompletionHandler:")
+            var noPushPayload = false;
+            if let options = launchOptions {
+                noPushPayload = options[UIApplicationLaunchOptionsRemoteNotificationKey] != nil;
+            }
+            if (preBackgroundPush || oldPushHandlerOnly || noPushPayload) {
+                PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+            }
+        }
+        let userNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound
+        let settings = UIUserNotificationSettings(forTypes: userNotificationTypes, categories: nil)
+        application.registerUserNotificationSettings(settings)
+        application.registerForRemoteNotifications()
         return true
     }
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        let installation = PFInstallation.currentInstallation()
+        installation.setDeviceTokenFromData(deviceToken)
+        installation.saveInBackground()
+    }
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        if error.code == 3010 {
+            println("Push notifications are not supported in the iOS Simulator.")
+        } else {
+            println("application:didFailToRegisterForRemoteNotificationsWithError: %@", error)
+        }
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        PFPush.handlePush(userInfo)
+        if application.applicationState == UIApplicationState.Inactive {
+            PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+        }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let destinationViewController = storyboard.instantiateViewControllerWithIdentifier("PushController") as! UIViewController
+        let tab = self.window?.rootViewController as! UINavigationController
+        tab.pushViewController(destinationViewController, animated: false)
+    }
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- LunchMore/ViewController.swift
+++ LunchMore/ViewController.swift
@@ -7,19 +7,84 @@
 //
 
 import UIKit
+import Parse
 
-class ViewController: UIViewController {
+extension UIColor{
+    class func randomColor(hue:CGFloat? = ( CGFloat( CGFloat(arc4random()) % 256 / 256.0 ) ),
+        saturation:CGFloat? = ( CGFloat( CGFloat(arc4random()) % 128 / 256.0 ) + 0.5 ),
+        brightness:CGFloat? = ( CGFloat( CGFloat(arc4random()) % 128 / 256.0 ) + 0.5 ),
+        alpha:CGFloat? = CGFloat(1.0)) -> UIColor{
+            return UIColor(hue: hue!, saturation: saturation!, brightness: brightness!, alpha: alpha!);
+    }
+}
 
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    var rowColors=[""]
+    var contacts = [Contact]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        rowColors=["90AAFF", "FFC090", "90FF06", "9A5E22", "FECB98", "55C7D4", "0EF699", "B96626"]
+        self.contacts = [Contact(name: "Alonzo"),Contact(name: "Chin"),Contact(name: "Jacob"),Contact(name: "Kendrick")]
+    
+        //Add background image to table view
+        //self.tableView.backgroundView = UIImageView(image: UIImage(named: "LunchBackground"))
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        tableView.rowHeight = 44.0
+        return self.contacts.count
+    }
+        //Function to use HEX Codes to create colors of the rows
+    func UIColorFromRGB(colorCode: String, alpha: Float = 1.0) -> UIColor {
+        var scanner = NSScanner(string:colorCode)
+        var color:UInt32 = 0;
+        scanner.scanHexInt(&color)
+        
+        let mask = 0x000000FF
+        let r = CGFloat(Float(Int(color >> 16) & mask)/255.0)
+        let g = CGFloat(Float(Int(color >> 8) & mask)/255.0)
+        let b = CGFloat(Float(Int(color) & mask)/255.0)
+        
+        return UIColor(red: r, green: g, blue: b, alpha: CGFloat(alpha))
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return CGFloat(100)
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        let randomIndex = Int(arc4random_uniform(UInt32(rowColors.count)))                          //Select random HEX code
+        var contact : Contact
+        
+        contact = contacts[indexPath.row]
+        cell.textLabel?.text = contact.name
+        cell.textLabel?.textColor = UIColor.whiteColor()
+        cell.textLabel?.font = UIFont(name: "Avenir", size: 44.0)
+        cell.textLabel?.textAlignment = NSTextAlignment.Center
+        cell.contentView.backgroundColor = UIColorFromRGB(rowColors[randomIndex])                   //Random HEX code makes bakcground color
+        cell.backgroundView = UIImageView(image: UIImage(named: "LunchBackground"))
+        rowColors.removeAtIndex(randomIndex)
+        return cell
+    }
 
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //self.performSegueWithIdentifier("showDetails", sender: tableView)
+        var cell = tableView.cellForRowAtIndexPath(indexPath)
+        cell?.textLabel!.text = "Sent!"
+        let push = PFPush()
+        push.setMessage("Lunch?")
+        push.sendPushInBackground()
+    }
 
 }
 
